f5a1dc25584e60a8856de562e0fda3b705b92850
MatijaNovosel <mnovosel5@gmail.com>
2021-03-03 09:07:36 +0100

    Visual elements and app version

--- pios-frontend/src/App.vue
+++ pios-frontend/src/App.vue
@@ -1,21 +1,44 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" app>
-      Drawer content
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+      disable-resize-watcher
+      class="elevation-3 grey lighten-3"
+    >
+      <v-list>
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              Matija Novosel
+            </v-list-item-title>
+            <v-list-item-subtitle>mnovosel5@gmail.com</v-list-item-subtitle>
+          </v-list-item-content>
+          <v-list-item-action>
+            <v-icon>mdi-menu-down</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+      <v-divider />
+      <v-list nav dense>
+        <v-list-item-group v-model="selectedItem" color="primary">
+          <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
     </v-navigation-drawer>
-    <v-app-bar app class="white black--text">
-      <div class="d-flex align-center">
-        <span>Gym aplikacija</span>
-      </div>
+    <v-app-bar app class="white black--text elevation-1" clipped-left>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <span>Gym aplikacija</span>
       <v-spacer />
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <span>v{{ appVersion }}</span>
     </v-app-bar>
     <v-main>
       <HelloWorld />
@@ -32,7 +55,18 @@ export default {
     HelloWorld
   },
   data: () => ({
-    drawer: false
+    drawer: false,
+    appVersion: process.env.PACKAGE_VERSION,
+    selectedItem: 0,
+    items: [
+      { text: "My Files", icon: "mdi-folder" },
+      { text: "Shared with me", icon: "mdi-account-multiple" },
+      { text: "Starred", icon: "mdi-star" },
+      { text: "Recent", icon: "mdi-history" },
+      { text: "Offline", icon: "mdi-check-circle" },
+      { text: "Uploads", icon: "mdi-upload" },
+      { text: "Log out", icon: "mdi-logout" }
+    ]
   })
 };
 </script>

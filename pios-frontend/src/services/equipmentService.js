e817afa95229e3b54cc05abe414cc2e666f22ff8
MatijaNovosel <mnovosel5@gmail.com>
2021-04-07 23:52:00 +0200

    Equipment service methods

--- pios-frontend/src/services/equipmentService.js
+++ pios-frontend/src/services/equipmentService.js
@@ -9,4 +9,25 @@ export default {
       name
     });
   },
+  async createEquipment({ name, typeId }) {
+    return await axios.post("/equipment", {
+      name,
+      typeId
+    });
+  },
+  async assignToCurrentUser(equipmentId) {
+    return await axios.post(`/equipment/assign/${equipmentId}`);
+  },
+  async unassignFromUser(equipmentId) {
+    return await axios.post(`/equipment/unassign/${equipmentId}`);
+  },
+  async getEquipment(name, showOnlyUserEquipment, type) {
+    return await axios.get("/equipment", {
+      params: {
+        name,
+        showOnlyUserEquipment,
+        type
+      }
+    });
+  }
 };
